3a4b1778ee854b1d3968f5c70e6e3b8d7cbe25be
ThymonMaarsen <129858650+MeeryMuggletail@users.noreply.github.com>
2023-04-04 14:25:00 +0200

    Create settings_example.ts

---
+++ src/settings_example.ts
@@ -0,0 +1,12 @@
+export const conf = {
+  rcon_host: "127.0.0.1",
+  rcon_port: 25575,
+  rcon_password: "12345678",
+  server_port: 8080,
+  mqtt_address: "mqtt://127.0.0.1",
+  mqtt_port: 1883,
+  mqtt_user: "admin",
+  mqtt_password: "admin",
+  mqtt_topic_prefix: "",
+  script_output_folder: "C:\\Users\\Administrator\\AppData\\Roaming\\Factorio\\script-output\\",
+};
